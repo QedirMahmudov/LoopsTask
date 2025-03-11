ffbb266a67123e9c7acd99714c89208a933d2f39
QedirMahmudov <1qedirmahmudov@gmail.com>
2025-03-12 00:04:23 +0400

    LoopTasks

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿//Sual 1--------------------------------------------------------------
+//int num = Convert.ToInt32(Console.ReadLine());
+//int dividedNumCount = 0;
+
+//for (int i = 1; i <= num; i++)
+//{
+//    if (num % i == 0)
+//    {
+//        dividedNumCount++;
+//    }
+//}
+
+//if (dividedNumCount > 2)
+//{
+//    Console.WriteLine($"murekkebdir,bolenlerin sayi:{dividedNumCount}");
+//}
+//else'
+
+//{
+//    Console.WriteLine("Sadedir!");
+//}
+
+
+
+//Sual2-----------------------------------------------------------
+//int num = Convert.ToInt32(Console.ReadLine());
+//int strenghtOfNumber = 1;
+
+//for (int i = 0; i < num; i++)
+//{
+//    if (num != strenghtOfNumber)
+//    {
+//        strenghtOfNumber *= 2;
+//    }
+//}
+
+//if (num == strenghtOfNumber)
+//{
+//    Console.WriteLine("Quvvetidir");
+
+//}
+//else
+//{
+//    Console.WriteLine("Deyil");
+//}
+
+
+
+
+//Sual3----------------------------------------------------
+
+//Bu taski sinifde isleyib gostermisdim.
